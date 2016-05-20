32dde9c873894db8e481ec1cc96f4bc9e49d5733
Asus <347253494@qq.com>
2016-05-20 10:21:54 +0800

    控件中画圆形，波浪动画要再画一个圆，设置叠加要在画笔上设置

--- app/src/main/java/com/example/asus/customview/MyViewActivity.java
+++ app/src/main/java/com/example/asus/customview/MyViewActivity.java
@@ -2,12 +2,25 @@ package com.example.asus.customview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.asus.customview.widget.MyView;
 
 public class MyViewActivity extends AppCompatActivity {
-
+    private Button mButton;
+    private MyView myView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view);
+        myView=(MyView)findViewById(R.id.myView);
+        mButton=(Button)findViewById(R.id.animotion);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myView.setAnimation();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/asus/customview/widget/MyView.java
+++ app/src/main/java/com/example/asus/customview/widget/MyView.java
@@ -1,21 +1,38 @@
 package com.example.asus.customview.widget;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.asus.customview.utils.DpUtils;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by Asus on 2016/5/19.
  */
 public class MyView extends View {
-    private Paint paint;
+    private Paint textPaint,circlePaint,pathPaint;
     private int width;
     private int height;
+    private float num=0;
+    private float offset=0;
+    private int num1=10,num2=20;
+    private Path path;
     public MyView(Context context) {
         super(context);
         init();
@@ -43,18 +60,84 @@ public class MyView extends View {
         height=getMeasuredHeight();
     }
     private void init(){
-        paint=new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.GREEN);
-        paint.setStrokeWidth(20f);
-        paint.setStyle(Paint.Style.STROKE);
+        circlePaint=new Paint();
+        circlePaint.setAntiAlias(true);
+        circlePaint.setColor(Color.GREEN);
+        circlePaint.setStrokeWidth(1);
+        circlePaint.setStyle(Paint.Style.FILL);
+        textPaint=new Paint();
+        textPaint.setStrokeWidth(1);
+        textPaint.setTextSize(DpUtils.dip2px(getContext(),20));
+        textPaint.setColor(Color.WHITE);
+
+        pathPaint=new Paint();
+        pathPaint.setStyle(Paint.Style.FILL);
+        pathPaint.setColor(Color.BLUE);
+        pathPaint.setStrokeWidth(1);
+        pathPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        path=new Path();
+        sendMessage();
+    }
+    private void setNum(float num){this.num=num;}
+    public void setAnimation(){
+        ObjectAnimator objectAnimation=ObjectAnimator.ofFloat(this,"num",0,100);
+        objectAnimation.setDuration(10000);
+        objectAnimation.start();
+        objectAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Log.d("数值",animation.getAnimatedValue()+"");
+               setNum ((Float) animation.getAnimatedValue()) ;
+
+                invalidate();
+            }
+        });
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(width/2,height/2,100f,paint);
+        canvas.drawCircle(width/2,height/2,height/2,circlePaint);
+        int save=canvas.saveLayer(0,0,width,height,textPaint,Canvas.ALL_SAVE_FLAG);
+        canvas.drawCircle(width/2,height/2,height/2,circlePaint);
+        path.reset();
+        path.moveTo(0,height);
+        path.lineTo(width,height);
+        path.lineTo(width+offset,height-height*num/100);
+        Log.d("d值",num+"");
+        for (int i = 0; i < width/num2 ;i++) {
+            path.rQuadTo(-num2,-num1,-num2*5,0);
+            path.rQuadTo(-num2,num1,-num2*2,0);
+
+        }
+        path.close();
+        canvas.drawPath(path,pathPaint);
+        canvas.restoreToCount(save);
+        DecimalFormat fnum=new DecimalFormat("##0.00");
+        String percent=fnum.format(num);
+        canvas.drawText(percent+"%",width/2,height/2,textPaint);
     }
 
+    Handler mHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 1:
+                    offset=+5;
+                    if (offset>num2*2){
+                        offset=0;
+                    }
+                    invalidate();
+                    sendEmptyMessageDelayed(1,50);
+                    break;
+            }
+        }
+    };
+public void sendMessage(){
+    Message message=new Message();
+    message.what=1;
+    mHandler.sendMessage(message);
+}
 }
